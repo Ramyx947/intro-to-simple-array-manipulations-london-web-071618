ea4801f7b311e4ca8c3dd0e75d10ef31c6be6da7
Ramona M <r1mari@yahoo.com>
2018-07-12 07:53:57 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -69,11 +69,15 @@ def using_flatten(private_colleges_in_newyork)
 private_colleges_in_newyork.flatten 
 end
 
-def using_delete(instructors, no_offense_steven)
-  instructors=["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = instructors.delete("Steven")
+# def using_delete(instructors, no_offense_steven)
+#   instructors=["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+#   no_offense_steven = instructors.delete("Steven")
 
+# end
+def using_delete(instructors, no_offense_steven)
+  instructors.delete(no_offense_steven)
 end
+
 
 def using_delete_at(famous_robots, deleted_robot)
   famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]

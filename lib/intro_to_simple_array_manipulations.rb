b5cc0e9d3ade8200b217ee99a140f08728c110a2
Ramona M <r1mari@yahoo.com>
2018-07-12 07:52:34 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,7 @@ end
 #   chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
 #   chars_in_game_of_thrones.pop(2)
 # end
+
 def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones.pop(2)
 end
@@ -40,11 +41,16 @@ end
   brands_removed = ice_cream_brands.shift(2)
 end
   
-def using_concat( my_favorite_things, more_favs)
-my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-more_favs = ["mario kart", "flatiron school"]
-my_favorite_things.concat(more_favs) 
+# def using_concat( my_favorite_things, more_favs)
+# my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+# more_favs = ["mario kart", "flatiron school"]
+# my_favorite_things.concat(more_favs) 
+# end
+
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
 end
+  
 
 def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
   list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]

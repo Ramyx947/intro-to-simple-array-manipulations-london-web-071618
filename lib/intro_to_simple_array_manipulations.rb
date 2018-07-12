7fb453b93ef50eac7834c961d6e3800e1d281b81
Ramona M <r1mari@yahoo.com>
2018-07-12 07:51:18 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,9 @@ end
 #   chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
 #   chars_in_game_of_thrones.pop(2)
 # end
+def pop_with_args(chars_in_game_of_thrones)
+  chars_in_game_of_thrones.pop(2)
+end
 
 # def using_shift(my_favorite_cities)
 #   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
